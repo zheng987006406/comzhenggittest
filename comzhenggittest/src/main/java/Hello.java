853ac7b15a6bb94ba2b3d4fa1bdbf9be9b0f9a8c
zheng <987006406@qq.com>
2020-09-27 12:22:38 +0800

    更新2

--- comzhenggittest/src/main/java/Hello.java
+++ comzhenggittest/src/main/java/Hello.java
@@ -7,5 +7,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("更新1");
+        System.out.println("更新2");
     }
 }
